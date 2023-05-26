ded0962302817007904240706e2272c0971cdb96
Victor Valar <valar@victorvalar.me>
2023-05-26 12:19:37 -0300

    add meta event data processing

---
+++ app/app.py
@@ -0,0 +1,56 @@
+import logging
+from facebook_business.adobjects.serverside.user_data import UserData
+from facebook_business.adobjects.serverside.content import Content
+from facebook_business.adobjects.serverside.custom_data import CustomData
+from facebook_business.adobjects.serverside.event import Event
+from facebook_business.adobjects.serverside.action_source import ActionSource
+from . import meta
+
+def event_handler(payload):
+    """
+    Sends events to Facebook Ads
+    """
+
+    if not payload.get('user_data').get('user_id'):
+        logging.error('##### No user_id found in payload')
+        raise ValueError('No user_id found in payload')
+
+
+    user_data = UserData(
+        external_id=payload.get('user_data').get('user_id'),
+    )
+
+    items = payload.get('items')
+    contents = []
+
+    for item in items:
+        content = Content(
+            product_id=item.get('item_id'),
+            quantity=item.get('quantity'),
+            item_price=item.get('price'),
+            brand=item.get('item_brand'),
+            title=item.get('item_name'),
+            category=item.get('item_category')
+        )
+        contents.append(content)
+
+    custom_data = CustomData(
+        contents=contents,
+        content_type='product',
+        currency='BRL',
+        value=payload.get('user_data').get('transaction_value'),
+        order_id=payload.get('user_data').get('transaction_id'),
+        custom_properties={'payment_type': payload.get('user_data').get('payment_type')}
+
+    )
+
+    event = Event(
+        event_name='purchase',
+        event_time=int(payload.get('user_data').get('transaction_time')),
+        user_data=user_data,
+        custom_data=custom_data,
+        action_source=ActionSource.SYSTEM_GENERATED,
+        event_id=payload.get('user_data').get('transaction_id'),
+    )
+
+    return meta.send_events([event])
